6c7490f42cc1001c8c246bf63406bcdcaca960d8
chno2632 <31612598+chno2632@users.noreply.github.com>
2024-05-24 18:14:43 +0200

    Added some input/output

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("Hello")
+name = input("Pleas enter your name? ")
+print("Hello " + name)
+
+number = input("What is your favourite number? ")
+int_number = int(number)
+print("Your number multiplicated by three is " + str(int_number * 3))
